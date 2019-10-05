63b7f4cf9c1924a29a242cfdd36f0154d1bcc299
Tran <trakhak@dunwoody.edu>
2019-10-04 23:45:41 -0500

    made progress2

--- Program.cs
+++ Program.cs
@@ -8,29 +8,46 @@ namespace humboldt_home_sales_assignment_2_trakhak
         static void Main(string[] args)
         {
             //DECLARATIONS
-            string initial; 
+            string initial;
             double total;
+
+            int sale;
 
             int SilasTotal, NoahTotal, ChrisTotal;
             int sum = Silas + Noah + Chris;
-            string EXIT = "Z";
+
+            string char = "S" | "s" | "N" | "n" | "C" | "c";
+            string EXIT = "Z" | "z";
 
             Console.WriteLine("Welcome to Humboldt Home Sales Agent Performance Calculator");
             Console.WriteLine("Please enter initials");
             initial = Console.ReadLine();
 
-            while (initial == "C" || "c")
+            while (initial != EXIT)
+            {
 
-                total = SilasTotal + NoahTotal + ChrisTotal
+                while (initial != char)
+                {
+                    Console.WriteLine("Invalid initials, please enter initials");
+                    initial = Console.ReadLine();
 
-                Console.WriteLine("Enter initials");
-                
+                }
+
+                Console.WriteLine($"Your Initials are {initial}. Please Enter amount of sale:");
+                sale = Convert.ToInt32(Console.ReadLine());
+
+                while (sale != EXIT)
+                {
+                    Console.WriteLine("Enter amount of sales");
+                    sale = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Your total sales is {total}. now lets show who had the highest total");
+
+                Console.WriteLine("To run the program again please enter initials");
+                initial = Convert.ToInt32(Console.ReadLine());
             }
 
-            Console.WriteLine("total all together =" + total "Silas =" + SilasTotal + "Noah =" + NoahTotal + "Chris =" + ChrisTotal);
-
-            Console.WriteLine("Program has concluded");
 
         }
-    }
-}
+
